c8306b15457d29923b44f4636b334590ec0d1c3f
Weihongliu <weihongliu@vsoontech.com>
2018-11-06 18:11:39 +0800

    Camera2Video
    学习Camera2

---
+++ android-Camera2Video-master/Application/src/main/java/com/example/android/camera2video/service/BackRecordService.java
@@ -0,0 +1,367 @@
+package com.example.android.camera2video.service;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.MediaRecorder;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.widget.Toast;
+
+import com.example.android.camera2video.event.StartEvent;
+import com.example.android.camera2video.event.StopEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class BackRecordService extends Service {
+
+    private Semaphore mCameraOpenCloseLock = new Semaphore(1);
+    private String mNextVideoAbsolutePath;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private Size mVideoSize;
+    private MediaRecorder mMediaRecorder;
+    private static final String[] VIDEO_PERMISSIONS = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private CameraDevice mCameraDevice;
+    private CameraCaptureSession mPreviewSession;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onLog("onStartCommand !");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.getDefault().register(this);
+        onLog("onCreate !");
+        openCamera();
+        startBackgroundThread();
+    }
+
+    //    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onStartEvent(StartEvent event) {
+        startRecordingVideo();
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onStopEvent(StopEvent event) {
+        stopRecordingVideo();
+        closeCamera();
+        stopBackgroundThread();
+        stopSelf();
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void openCamera() {
+        if (hasPermissionsGranted(VIDEO_PERMISSIONS)) {
+            CameraManager manager = (CameraManager) getApplicationContext().getSystemService(Context.CAMERA_SERVICE);
+            try {
+                onLog("tryAcquire");
+                if (!mCameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
+                    throw new RuntimeException("Time out waiting to lock camera opening.");
+                }
+                String cameraId = manager.getCameraIdList()[0];
+
+                onLog("cameraId = " + cameraId);
+                // Choose the sizes for camera preview and video recording
+                CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics
+                        .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                if (map == null) {
+                    throw new RuntimeException("Cannot get available preview/video sizes");
+                }
+                mVideoSize = chooseVideoSize(map.getOutputSizes(MediaRecorder.class));
+                onLog("mVideoSize = " + mVideoSize.toString());
+                mMediaRecorder = new MediaRecorder();
+                manager.openCamera(cameraId, mStateCallback, null);
+            } catch (CameraAccessException e) {
+                onLog("Exception  Cannot access the camera.");
+            } catch (NullPointerException e) {
+                onLog("Exception = " + e.getMessage());
+            } catch (InterruptedException e) {
+                onLog("Exception  Interrupted while trying to lock camera opening.");
+            }
+        } else {
+            onLog("No Permissions !" + VIDEO_PERMISSIONS.toString());
+        }
+    }
+
+    private static void onLog(String log) {
+        Log.d("BackRecordService", log);
+    }
+
+    private boolean hasPermissionsGranted(String[] permissions) {
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(getApplicationContext(), permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+            onLog("StateCallback onOpened !");
+            mCameraDevice = cameraDevice;
+            mCameraOpenCloseLock.release();
+
+            startRecordingVideo();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            onLog("StateCallback onDisconnected !");
+            mCameraOpenCloseLock.release();
+            cameraDevice.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int error) {
+            onLog("StateCallback onError !");
+            mCameraOpenCloseLock.release();
+            cameraDevice.close();
+            mCameraDevice = null;
+        }
+
+    };
+
+    private void startPreview() {
+        if (null == mCameraDevice) {
+            return;
+        }
+        try {
+            closePreviewSession();
+            mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            List<Surface> surfaces = new ArrayList<>();
+            // Set up Surface for the MediaRecorder
+            Surface recorderSurface = mMediaRecorder.getSurface();
+            surfaces.add(recorderSurface);
+            mCameraDevice.createCaptureSession(surfaces,
+                    new CameraCaptureSession.StateCallback() {
+
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            mPreviewSession = session;
+                            updatePreview();
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                        }
+                    }, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static Size chooseVideoSize(Size[] choices) {
+        for (Size size : choices) {
+            if (size.getWidth() == size.getHeight() * 4 / 3 && size.getWidth() <= 1080) {
+                return size;
+            }
+        }
+        onLog("Couldn't find any suitable video size");
+        return choices[choices.length - 1];
+    }
+
+    private void setUpMediaRecorder() throws IOException {
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        if (mNextVideoAbsolutePath == null || mNextVideoAbsolutePath.isEmpty()) {
+            mNextVideoAbsolutePath = getVideoFilePath(getApplicationContext());
+        }
+        mMediaRecorder.setOutputFile(mNextVideoAbsolutePath);
+        mMediaRecorder.setVideoEncodingBitRate(10000000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(mVideoSize.getWidth(), mVideoSize.getHeight());
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        mMediaRecorder.setOrientationHint(90);
+        mMediaRecorder.prepare();
+    }
+
+    private String getVideoFilePath(Context context) {
+        final File dir = context.getExternalFilesDir(null);
+        return (dir == null ? "" : (dir.getAbsolutePath() + "/"))
+                + System.currentTimeMillis() + ".sy";
+    }
+
+    private void stopRecordingVideo() {
+        try {
+            mMediaRecorder.stop();
+            mMediaRecorder.reset();
+            onLog("Video saved: " + mNextVideoAbsolutePath);
+            mNextVideoAbsolutePath = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startRecordingVideo() {
+        if (null == mCameraDevice) {
+            return;
+        }
+        try {
+            onLog("startRecordingVideo !");
+            closePreviewSession();
+            setUpMediaRecorder();
+            createCaptureSession();
+        } catch (CameraAccessException | IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void createCaptureSession() throws CameraAccessException {
+        mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+        List<Surface> surfaces = new ArrayList<>();
+        Surface recorderSurface = mMediaRecorder.getSurface();
+        surfaces.add(recorderSurface);
+        mPreviewBuilder.addTarget(recorderSurface);
+        mCameraDevice.createCaptureSession(surfaces, new CameraCaptureSession.StateCallback() {
+
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                mPreviewSession = cameraCaptureSession;
+                updatePreview();
+                mBackgroundHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mMediaRecorder.start();
+                    }
+                });
+                onLog("CameraDevice onConfigured ! Start recording !");
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                onLog("CameraDevice onConfigureFailed !");
+            }
+        }, mBackgroundHandler);
+    }
+
+
+    private void updatePreview() {
+        if (null == mCameraDevice) {
+            return;
+        }
+        try {
+            setUpCaptureRequestBuilder(mPreviewBuilder);
+            HandlerThread thread = new HandlerThread("CameraPreview");
+            thread.start();
+            mPreviewSession.setRepeatingRequest(mPreviewBuilder.build(), null, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setUpCaptureRequestBuilder(CaptureRequest.Builder builder) {
+        builder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+    }
+
+    private void closePreviewSession() {
+        if (mPreviewSession != null) {
+            mPreviewSession.close();
+            mPreviewSession = null;
+        }
+    }
+
+    /**
+     * Starts a background thread and its {@link Handler}.
+     */
+    private void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("CameraBackground");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    /**
+     * Stops the background thread and its {@link Handler}.
+     */
+    private void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+        onLog("onDestroy !");
+    }
+
+    private void closeCamera() {
+        try {
+            mCameraOpenCloseLock.acquire();
+            closePreviewSession();
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                mCameraDevice = null;
+            }
+            if (null != mMediaRecorder) {
+                mMediaRecorder.release();
+                mMediaRecorder = null;
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Interrupted while trying to lock camera closing.");
+        } finally {
+            mCameraOpenCloseLock.release();
+        }
+    }
+}
